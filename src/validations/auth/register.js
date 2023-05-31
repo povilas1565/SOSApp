2e0d0d4fcc5c2cd4c006888bffc1059eb123f54e
Paul156551 <pavel.ryzhkov.99@mail.ru>
2023-05-31 11:57:28 +0300

    Commit Changes.

--- src/validations/auth/register.js
+++ src/validations/auth/register.js
@@ -3,6 +3,6 @@ module.exports = validate({
     body: Joi.object({
         email: Joi.string().email().required(),
         name: Joi.string().required(),
-        pass: Joi.string().min(6).required()
+        password: Joi.string().min(6).required()
     })
 });
